4c8676adaf3a29139c53a036884c2810b9f30421
lmcuende <lmcuende@gmail.com>
2022-10-03 22:18:29 +0100

    Updated until Section 4

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -11,14 +11,47 @@ import * as moment from 'moment';
 })
 export class CourseDialogComponent implements OnInit {
 
-    constructor(private fb: FormBuilder) {
+    description: string;
 
+    form = this.fb.group({
+        description: [this.course.description, Validators.required],
+        category: [this.course.category, Validators.required],
+        releasedAt: [new Date(), Validators.required],
+        longDescription: [this.course.longDescription, Validators.required]
+    });
 
+    constructor(private fb: FormBuilder, 
+                @Inject(MAT_DIALOG_DATA) private course: Course,
+                private dialogRef: MatDialogRef<CourseDialogComponent>) {
+        this.description = course.description;
     }
 
     ngOnInit() {
 
     }
 
+    close() {
+        this.dialogRef.close();
+    }
+
+    save() {
+        this.dialogRef.close(this.form.value);
+    }
+
 }
 
+export function openEditCourseDialog(dialog: MatDialog, course: Course) {
+    const config = new MatDialogConfig();
+
+    config.disableClose = true;
+    config.autoFocus = true;
+
+    config.data = {
+        ...course
+    };
+
+    const dialogRef = dialog.open(CourseDialogComponent, config);
+
+    return dialogRef.afterClosed();
+
+}
